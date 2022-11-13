2972d32f404c38e2a1be7700555c1ca41b402cbc
Hugo R Alves <r.hugoalves66@gmail.com>
2022-11-13 14:23:11 -0300

    Funções e Variáveis / Variables and Functions

---
+++ main.py
@@ -0,0 +1,15 @@
+# Rock-Paper-Scissor Intro Project / Projeto Introdutório: Pedra, Papel e Tesoura #
+# FreeCodeCamp
+# Hugo R. Alves https://github.com/r-hugoalves/Rock-Paper-Scissors-Intro-Project
+
+            # Funções e Variáveis / Variables and Functions #
+
+def get_choices():
+    player_choice = 'rock'
+    computer_choice = 'paper'
+
+    return player_choice
+
+print(get_choices())
+
+
