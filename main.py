396b7ba8f0cde40df2b0e026e710faa5f53b8ec2
Hugo R Alves <r.hugoalves66@gmail.com>
2022-11-13 14:53:22 -0300

    Input Choice and Dictionaries

--- main.py
+++ main.py
@@ -5,11 +5,11 @@
             # Funções e Variáveis / Variables and Functions #
 
 def get_choices():
-    player_choice = 'rock'
+    player_choice = input('Escolha uma das opções (pedra, papel ou tesoura): ')
     computer_choice = 'paper'
+    choices = {'player': player_choice, 'computer': computer_choice}
+        # 'choice' é um dicionário / dict = {'key': value, 'key': value}
 
-    return player_choice
+    return choices
 
 print(get_choices())
-
-
